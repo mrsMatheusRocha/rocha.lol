f7a6f76c718b034212351699e7aa52beeff4f8b7
Matheus Rocha Sousa <166670004+mrsMatheusRocha@users.noreply.github.com>
2025-08-21 23:32:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-	res.write("On the way to being a front end engineer!");
+	res.write("Teste");
 	res.end();
 }).listen(3000);
 
