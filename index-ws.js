8cc26d3307a56029bd3216d54a75e2d95cc4ac0d
Matheus Rocha Sousa <166670004+Marthplays@users.noreply.github.com>
2025-08-23 21:00:55 -0300

    Add initial CSS styles for layout, typography, and responsive design

--- index-ws.js
+++ index-ws.js
@@ -3,6 +3,9 @@ const server = require('http').createServer();
 const app = express();
 const PORT = 3000;
 
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/styles', express.static(__dirname + '/styles'));
+
 app.get('/', function(req, res) {
   res.sendFile('index.html', {root: __dirname});
 });
@@ -10,6 +13,15 @@ app.get('/', function(req, res) {
 server.on('request', app);
 
 server.listen(PORT, function () { console.log('Listening on ' + PORT); });
+
+process.on('SIGINT', () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  })
+  server.close(() => {
+    shutdownDB();
+  })
+})
 
 /** Websocket **/
 const WebSocketServer = require('ws').Server;
@@ -26,6 +38,8 @@ wss.on('connection', function connection(ws) {
   if (ws.readyState === ws.OPEN) {
     ws.send('Welcome to my server!');
   }
+
+  db.run(`INSERT INTO visitors (count, time) VALUES (${numClients}, datetime('now'))` )
 
   ws.on('close', function close() {
     wss.broadcast(`Current visitors: ${wss.clients.size}`);
@@ -49,3 +63,28 @@ wss.broadcast = function broadcast(data) {
   });
 };
 /** End Websocket **/
+
+/** Begin DataBase **/
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+  db.run(`
+      CREATE TABLE visitors (
+        count INTEGER,
+        time TEXT
+      )
+    `)
+})
+
+function getCounts() {
+  db.each('SELECT * FROM visitors', (err, row) => {
+    console.log(row);
+  })
+}
+
+function shutdownDB() {
+  getCounts();  
+  console.log('Shutting down database');
+}
+
